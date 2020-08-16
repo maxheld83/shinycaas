ef28c91ab59680d276edfd8d138e15fb5f219fc6
Maximilian Held <info@maxheld.de>
2020-08-16 19:46:45 +0200

    fix sha env var

--- R/helpers.R
+++ R/helpers.R
@@ -1,6 +1,6 @@
 #' Use `github.sha` environment variable or `"latest"` as docker image tag
 #'
-#' Retrieves the current git commit SHA from the [`github.sha`](https://docs.github.com/en/actions/reference/context-and-expression-syntax-for-github-actions) environment variable set on GitHub Actions.
+#' Retrieves the current git commit SHA from the [`GITHUB_SHA`](https://docs.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables) environment variable set on GitHub Actions.
 #' Returns `latest`, if the environment variable is an empty string.
 #' Internal function used to retrieve the appropriate docker image tag for a shiny app.
 #'
@@ -10,7 +10,7 @@
 #'
 #' @keywords internal
 get_tag <- function() {
-  sha <- Sys.getenv("gihub.sha")
+  sha <- Sys.getenv("GITHUB_SHA")
   if (sha == "") {
     tag <- "latest"
   } else {
